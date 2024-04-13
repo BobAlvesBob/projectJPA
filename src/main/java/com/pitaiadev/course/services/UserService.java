e8389ef262f77ab6dc6bdc40cb72e5f4c2317e9e
Robson Alves <bobpitaia@gmail.com>
2024-04-13 20:43:29 -0300

    Service layer, component registration

---
+++ src/main/java/com/pitaiadev/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.pitaiadev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pitaiadev.course.entities.User;
+import com.pitaiadev.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj=repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
